39cf1aa821559f663f20039804f9bf2803265b4d
anton-kotau <kot98vaz@gmail.com>
2024-05-30 18:22:58 +0200

    feat(GUI.py): add new buttons

--- GUI.py
+++ GUI.py
@@ -1,5 +1,10 @@
 from tkinter import *
 import customtkinter
+import sql
+from tkinter import messagebox, filedialog
+# import ttkbootstrap as tb
+from datetime import datetime
+
 
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
@@ -7,39 +12,97 @@ customtkinter.set_default_color_theme("dark-blue")
 root = customtkinter.CTk()
 
 
-root.title('SQL_base')
+root.title('PersonDB')
 root.geometry('800x500')
 root.eval('tk::PlaceWindow . center')
 
 frame = customtkinter.CTkFrame(root, width=300, height=150, corner_radius=10)
 frame.pack(pady=20, padx=20, fill='both', expand=True)
 
+
 def hide_main():
-    button.place_forget()
-    button2.place_forget()
-    button3.place_forget()
-    button4.place_forget()
-    button5.place_forget()
-    
+    button_open.place_forget()
+    button_create.place_forget()
+    button_settings.place_forget()
+    button_show_about.place_forget()
+    button_exit.place_forget()
 
-def show_main():
-    button.place(x=40, y=40)
-    button2.place(x=40, y=80)
-    button3.place(x=40, y=120)
-    button4.place(x=40, y=160)
-    button5.place(x=40, y=200)
-    back_to_main.place_forget()
+def show_main_from_create():
+    button_open.place(x=40, y=40)
+    button_create.place(x=40, y=80)
+    button_settings.place(x=40, y=120)
+    button_show_about.place(x=40, y=160)
+    button_exit.place(x=40, y=200)
+    passport_entry.place_forget()
+    theme_button.place_forget()
 
-def settings_buttons():
+def show_main_from_settings():
+    button_open.place(x=40, y=40)
+    button_create.place(x=40, y=80)
+    button_settings.place(x=40, y=120)
+    button_show_about.place(x=40, y=160)
+    button_exit.place(x=40, y=200)
+    back_to_main_from_settings.place_forget()
+    theme_button.place_forget()
+
+
+
+# CREATE
+
+
+def create_person():
+    passport = passport_entry.get()
+    photo = photo_entry.get()
+    firstname = firstname_entry.get()
+    lastname = lastname_entry.get()
+    gender = gender_entry.get()
+    phone_number = phone_number_entry.get()
+    date_of_birthday = tb.DateEntry(root, bootstyle='danger')
+    date_of_birthday.place(y=50, x=50)
+    notes = notes_entry.get()
+
+    if not passport or not photo or not firstname or not lastname or not gender or not phone_number or not date_of_birthday:
+        messagebox.showerror('Bład', 'Wszystkie pola muszą być wypełnione.')
+        return
+
+# creation of input fields
+
+
+def create():
     hide_main()
-    theme_button.place(x=40, y=40)
-    back_to_main.place(x=40, y=350)
-    
+    passport_entry = customtkinter.CTkEntry(
+        frame, placeholder_text="Passport ID")
+    photo_entry = customtkinter.CTkEntry(frame, placeholder_text="link")
+    firstname_entry = customtkinter.CTkEntry(
+        frame, placeholder_text="firstname")
+    lastname_entry = customtkinter.CTkEntry(frame, placeholder_text="lastname")
+    gender_entry = customtkinter.CTkEntry(frame, placeholder_text="gender")
+    phone_number_entry = customtkinter.CTkEntry(
+        frame, placeholder_text="phone number")
+    date_of_birthday_entry = customtkinter.CTkEntry(
+        frame, placeholder_text="date of birthday")
+    notes_entry = customtkinter.CTkEntry(frame, placeholder_text="Notes")
+    passport_entry.place(x=460, y=40)
+    photo_entry.place(x=460, y=80)
+    firstname_entry.place(x=460, y=120)
+    lastname_entry.place(x=460, y=160)
+    gender_entry .place(x=460, y=200)
+    phone_number_entry.place(x=460, y=240)
+    date_of_birthday_entry.place(x=460, y=280)
+    notes_entry.place(x=460, y=320)
+    back_to_main_from_settings.place(x=40, y=350)
+
+# SETTINGS
+
 
 def settings():
-    settings_buttons()
+    hide_main()
+    theme_button.place(x=40, y=40)
+    back_to_main_from_settings.place(x=40, y=350)
+
 
 mode = 'dark'
+
 
 def change_theme():
     global mode
@@ -49,6 +112,8 @@ def change_theme():
     else:
         customtkinter.set_appearance_mode('dark')
         mode = 'dark'
+
+# ABOUT
 
 
 def show_about():
@@ -68,32 +133,35 @@ def show_about():
         window_about, wraplength=250, text='PersonDB is an application for browsing databases of people. The application allows you to add new records, search for existing records and change personal data. Version 0.0.0.1')
     label.pack(padx=0.5, pady=40)
 
+# EXIT
+
 
 def close_app():
     root.destroy()
 
 
-button = customtkinter.CTkButton(
+# ALL_BUTTONS
+button_open = customtkinter.CTkButton(
     frame, text='Open', command=lambda: print('Działa'))
-button.place(x=40, y=40)
-button2 = customtkinter.CTkButton(
-    frame, text='Create', command=lambda: print('Działa'))
-button2.place(x=40, y=80)
-button3 = customtkinter.CTkButton(
+button_open.place(x=40, y=40)
+button_create = customtkinter.CTkButton(
+    frame, text='Create', command=create)
+button_create.place(x=40, y=80)
+button_settings = customtkinter.CTkButton(
     frame, text='Settings', command=settings)
-button3.place(x=40, y=120)
-button4 = customtkinter.CTkButton(
+button_settings.place(x=40, y=120)
+button_show_about = customtkinter.CTkButton(
     frame, text='About', command=show_about)
-button4.place(x=40, y=160)
-button5 = customtkinter.CTkButton(
+button_show_about.place(x=40, y=160)
+button_exit = customtkinter.CTkButton(
     frame, text='Exit', command=close_app)
-button5.place(x=40, y=200)
+button_exit.place(x=40, y=200)
 theme_button = customtkinter.CTkButton(
     frame, text='Change theme', command=change_theme)
-back_to_main = customtkinter.CTkButton(
-    frame, text='Back', command=show_main)
-
-
+back_to_main_from_settings = customtkinter.CTkButton(
+    frame, text='Back', command=show_main_from_settings)
+back_to_main_from_create = customtkinter.CTkButton(
+    frame, text='Back', command=show_main_from_create)
 
 
 root.mainloop()
